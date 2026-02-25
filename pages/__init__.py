430229f6165a8d620caae20547f907fe6e2ac24f
gangstamood <57985325+gangstamood@users.noreply.github.com>
2026-02-25 19:22:43 +0100

    Delete pages directory

--- pages/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Package pages - Modules de pages de l'application.
-"""
-from . import dashboard
-from . import vehicules
-from . import scooters
-from . import engins
-from . import parametres
